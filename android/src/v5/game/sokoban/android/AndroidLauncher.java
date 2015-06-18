cabc61a9f536d01d81dfe9e7b8c930f4d6e7ca83
v5v5 <v5@hotbox.ru>
2015-06-18 16:28:34 +0300

    start

---
+++ android/src/v5/game/sokoban/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package v5.game.sokoban.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import v5.game.sokoban.SokobanGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SokobanGame(), config);
+	}
+}
---
+++ desktop/src/v5/game/sokoban/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package v5.game.sokoban.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import v5.game.sokoban.SokobanGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SokobanGame(), config);
+	}
+}
