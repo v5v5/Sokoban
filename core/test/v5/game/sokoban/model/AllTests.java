ebb06fdc598227a46945a57c89ca95cf23172c39
v5v5 <v5@hotbox.ru>
2015-06-19 10:55:01 +0300

    added AllTests

---
+++ core/test/v5/game/sokoban/model/AllTests.java
@@ -0,0 +1,11 @@
+package v5.game.sokoban.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MoveBox.class, MoveMan.class, SokobanGameTest.class })
+public class AllTests {
+
+}
